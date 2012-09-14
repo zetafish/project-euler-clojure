a6641e0d156bd1fcc904fa079ab239f5cbc54119
Endymion Kasanardjo <zetafish@gmail.com>
2012-09-14 23:08:39 +0200

    solved p35

---
+++ src/project_euler/p053.clj
@@ -0,0 +1,16 @@
+(defn binomial
+  [n k]
+  (loop [n n k k acc 1]
+    (cond (or (= n k) (= 0 k)) acc
+          :else (recur (dec n) (dec k) (* acc (/ n k))))))
+
+(defn coefs
+  [n]
+  (map #(binomial n %) (range (inc n))))
+
+(defn p35
+  [n]
+  (map #(->> (coefs %)
+             (filter (fn [c] (> c 1000000)))
+             count)
+       (range 1 (inc n))))
